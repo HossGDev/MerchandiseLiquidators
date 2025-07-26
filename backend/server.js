a7e065fee3b3df0a9daf7759ae880189865764c4
HossGDev <stuckinasimulation@protonmail.com>
2025-07-25 17:43:52 -0700

    compleete coupon routes and coupon controller

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
-import cartRoutes from "./routes/cart.route.js"
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,12 +14,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // allowys to to parse the body of request.
+app.use(express.json()); // allowys to parse the body of request.
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 console.log(process.env.PORT);
 
