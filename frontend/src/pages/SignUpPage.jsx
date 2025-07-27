bf98047a750388eb993426ba7d2720a52038f49f
HossGDev <stuckinasimulation@protonmail.com>
2025-07-27 12:07:24 -0700

    begin Frontend, install dependencies

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
+import { motion } from "framer-motion";
+import { useUserStore } from "../stores/useUserStore";
+
+const SignUpPage = () => {
+  const loading = true
+  return (
+    <div>SignUpPage</div>
+  )
+}
+
+export default SignUpPage
